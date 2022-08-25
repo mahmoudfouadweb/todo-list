13404d896c5124463b6bce5d5a35b9b48cd96b6f
mahmoud fouad <mfapolice@gmail.com>
2022-08-25 05:19:25 +0200

    update

--- src/component/listItem/ListItem.js
+++ src/component/listItem/ListItem.js
@@ -1,5 +1,9 @@
+import { useContext } from 'react';
+import TodoCtx from '../../store/context-Todo';
 import TodoItems from '../todoItems/TodoItems';
 function ListItems() {
+  const todoCtx = useContext(TodoCtx);
+  console.log(todoCtx);
   return (
     <>
       <h1>List Item:</h1>
--- src/store/context-Todo.js
+++ src/store/context-Todo.js
@@ -1,17 +1,28 @@
 import { createContext } from 'react';
 
 const TodoCtx = createContext({
-  item: [],
+  item: [{}],
+  addTodo: todoID => {},
+  removeTodo: todoID => {},
+  isTodoImportant: todos => {},
 });
 
 export function TodoContextProvider(props) {
+  function addTodo() {}
+  function removeTodo() {}
+  function isTodoImportant() {}
+
   const todos = {
     item: [
       { id: 1, name: 'mahmoud', age: 31 },
       { id: 1, name: 'rovy', age: 6 },
       { id: 1, name: 'joo', age: 4 },
     ],
+    addTodo: addTodo,
+    removeTodo: removeTodo,
+    isTodoImportant: isTodoImportant,
   };
+  console.log(todos);
   return <TodoCtx.Provider value={todos}>{props.children}</TodoCtx.Provider>;
 }
 
