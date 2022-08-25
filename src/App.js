8864e577999974bb1b8142bb2fec91ed94d0c5f7
mahmoud fouad <mfapolice@gmail.com>
2022-08-25 05:46:23 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import './App.css';
 import ListItem from './component/listItem/ListItem';
 
 function App() {
-  let state = {
-    state: [
-      { id: 1, name: 'hamza', age: 22 },
-      { id: 1, name: 'mohamed', age: 23 },
-      { id: 1, name: 'abdo', age: 24 },
-    ],
-  };
-
   return (
     <>
       <h1>TodoList App</h1>
