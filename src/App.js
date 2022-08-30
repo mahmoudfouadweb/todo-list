893820b97141f38982da7015b51b7a42b790e752
mahmoud fouad <mfapolice@gmail.com>
2022-08-30 18:57:50 +0200

    update

--- src/App.js
+++ src/App.js
@@ -9,74 +9,43 @@ import {
 import './App.css';
 
 function App() {
-  // task TodoList state
   const [todo, setTodo] = useState([
-    { id: 1, title: 'Task 1', status: false },
-    { id: 2, title: 'Task 2', status: false },
-    { id: 3, title: 'Task 3', status: false },
+    { id: 1, title: 'Task 1', done: false },
+    { id: 2, title: 'Task 2', done: false },
+    { id: 3, title: 'Task 3', done: false },
   ]);
-  const [newTask, setNewTask] = useState('');
-  const [updateTask, setUpdateTask] = useState('');
-  ///////////////////////////////////////
-  // add new task
-  function getNewTask() {
-    if (newTask) {
-      setTodo([
-        ...todo,
-        {
-          id: todo.length + 1,
-          title: newTask,
-          status: false,
-        },
-      ]);
-      setNewTask('');
-    }
-  }
-  ///////////////////////////////////////
-  // done undone
-  function taskDoneToggle(id) {
-    const tasks = todo.map(item => {
-      if (item.id === id) {
-        return {
-          ...item,
-          status: !item.status,
-        };
-      }
-      return item;
-    });
-    setTodo(tasks);
-    console.log(todo);
-  }
-  ///////////////////////////////////////
-  //delete task
-  function deleteItem(id) {
-    setTodo(todo.filter(item => item.id !== id));
-  }
-  ///////////////////////////////////////
-  //cancel BTN
-  function cancelUpdate() {
-    setUpdateTask('');
-  }
-  function updateTaskBtn() {
-    const filtered = [...todo.filter(item => item.id !== updateTask.id)];
-    setTodo([
-      ...filtered,
-      {
-        id: updateTask.id,
-        title: updateTask.title,
-        status: updateTask.status ? true : false,
-      },
-    ]);
-    setUpdateTask('');
-  }
-  function editTitleOnChange(newTitle) {
-    let newEntry = {
-      id: updateTask.id,
-      title: newTitle,
-      status: updateTask.status ? true : false,
-    };
-    setUpdateTask(newEntry);
-  }
+  const [] = useState('');
+  const [] = useState('');
+  /////////////////////////////////////////
+  //
+  const addTaskHandler = () => {
+    //
+  };
+  /////////////////////////////////////////
+  //
+  const updateTaskHandler = () => {
+    //
+  };
+  /////////////////////////////////////////
+  //
+  const cancelUpdateHandler = () => {
+    //
+  };
+  /////////////////////////////////////////
+  //
+  const completeTaskHandler = () => {
+    //
+  };
+  /////////////////////////////////////////
+  //
+  const editTaskHandler = () => {
+    //
+  };
+  /////////////////////////////////////////
+  //
+  const deleteTaskHandler = () => {
+    //
+  };
 
   return (
     <div className="App container">
@@ -85,49 +54,32 @@ function App() {
       <br></br>
       {/* Update task  */}
       {/* // update field */}
-      {updateTask.title ? (
-        <>
-          <div className="row">
-            <div className="col">
-              <input
-                className="form-control form-control-lg"
-                value={updateTask && updateTask.title}
-                onChange={e => editTitleOnChange(e.target.value)}
-              />
-            </div>
-            <div className="col-auto">
-              <button
-                className="btn btn-lg btn-success mr-20"
-                onClick={updateTaskBtn}
-              >
-                Update
-              </button>
-              <button className="btn btn-lg btn-warning" onClick={cancelUpdate}>
-                Cancel
-              </button>
-            </div>
+
+      <>
+        <div className="row">
+          <div className="col">
+            <input className="form-control form-control-lg" />
           </div>
-          <br />
-        </>
-      ) : (
-        <>
-          <div className="row">
-            <div className="col">
-              <input
-                className="form-control form-control-lg"
-                value={newTask}
-                onChange={e => setNewTask(e.currentTarget.value)}
-              />
-            </div>
-            <div className="col-auto">
-              <button className="btn btn-lg btn-success" onClick={getNewTask}>
-                Add Task
-              </button>
-            </div>
+          <div className="col-auto">
+            <button>Update</button>
+            <button className="btn btn-lg btn-warning">Cancel</button>
           </div>
-          <br />
-        </>
-      )}
+        </div>
+        <br />
+      </>
+
+      <>
+        <div className="row">
+          <div className="col">
+            <input className="form-control form-control-lg" />
+          </div>
+          <div className="col-auto">
+            <button className="btn btn-lg btn-success">Add Task</button>
+          </div>
+        </div>
+        <br />
+      </>
+
       {/* // new Entry Field */}
 
       {/* Add Task */}
@@ -143,25 +95,13 @@ function App() {
                 <span className="taskText">{task.title}</span>
               </div>
               <div className="iconsWrap">
-                <span
-                  title="Completed / Not Completed"
-                  onClick={() => taskDoneToggle(task.id)}
-                >
+                <span title="Completed / Not Completed">
                   <FontAwesomeIcon icon={faCircleCheck} />
                 </span>
-                <span
-                  title="Edit"
-                  onClick={() =>
-                    setUpdateTask({
-                      id: task.id,
-                      title: task.title,
-                      status: task.status ? true : false,
-                    })
-                  }
-                >
+                <span title="Edit">
                   <FontAwesomeIcon icon={faPen} />
                 </span>
-                <span title="Delete" onClick={() => deleteItem(task.id)}>
+                <span title="Delete">
                   <FontAwesomeIcon icon={faTrashCan} />
                 </span>
               </div>
